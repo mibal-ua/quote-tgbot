44fbbea7b46a31b6026e7b59fcd67d06f6698e49
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-07-12 14:43:05 +0200

    Enable scheduling

--- src/main/java/ua/mibal/bot/config/RootConfig.java
+++ src/main/java/ua/mibal/bot/config/RootConfig.java
@@ -2,12 +2,14 @@ package ua.mibal.bot.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ua.mibal.bot.config.props.BotProps;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
+@EnableScheduling
 @EnableConfigurationProperties({
         BotProps.class
 })
