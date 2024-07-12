f278c947c2583213a4905d958a4738e843c7e487
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-07-12 11:43:04 +0200

    Implement `MessageController`

---
+++ src/main/java/ua/mibal/bot/controller/MessageController.java
@@ -0,0 +1,25 @@
+package ua.mibal.bot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@RestController
+@RequestMapping("/api/message")
+public class MessageController {
+    private static final Logger log = LoggerFactory.getLogger(MessageController.class);
+
+    @RequestMapping(method = {GET, POST})
+    public void receiveMessage(@RequestBody Object message) {
+        log.info("Received message: {}", message);
+    }
+}
