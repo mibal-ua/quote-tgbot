3b57efac887e99af9ff7c5c1ed608ac59de147bc
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-07-12 13:50:54 +0200

    Add `TelegramMethodPerformer` logging

--- src/main/java/ua/mibal/bot/service/component/TelegramMethodPerformer.java
+++ src/main/java/ua/mibal/bot/service/component/TelegramMethodPerformer.java
@@ -1,5 +1,6 @@
 package ua.mibal.bot.service.component;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ua.mibal.bot.config.props.BotProps;
@@ -14,6 +15,7 @@ import static org.springframework.http.HttpMethod.POST;
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
+@Slf4j
 @Component
 public class TelegramMethodPerformer {
     private final RestTemplate restTemplate = new RestTemplate();
@@ -24,6 +26,8 @@ public class TelegramMethodPerformer {
     }
 
     public void perform(Method method, Map<String, Object> params) {
+        log.info("Perform operation {}, params: {}", method, params);
+        
         method.validateParams(params);
         String url = format(urlFormat, method.getMethodName());
         restTemplate.execute(url, POST, null, null, params);
