0af2f5be6c98896dcd41eaca5b5faef14e2db39a
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-07-12 15:56:33 +0200

    Implement `PhotoSender`

--- src/main/java/ua/mibal/bot/model/Method.java
+++ src/main/java/ua/mibal/bot/model/Method.java
@@ -5,7 +5,8 @@ package ua.mibal.bot.model;
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
 public enum Method {
-    SEND_MESSAGE("sendMessage?chat_id={chat_id}&text={text}");
+    SEND_MESSAGE("sendMessage?chat_id={chat_id}&text={text}"),
+    SEND_ANIMATION("sendAnimation?chat_id={chat_id}&animation={animation}");
 
     private final String methodTemplate;
 
--- src/main/java/ua/mibal/bot/service/component/PhotoSender.java
+++ src/main/java/ua/mibal/bot/service/component/PhotoSender.java
@@ -1,16 +1,30 @@
 package ua.mibal.bot.service.component;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ua.mibal.bot.model.Photo;
+
+import java.util.Map;
+
+import static ua.mibal.bot.model.Method.SEND_ANIMATION;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class PhotoSender {
+    private final TelegramMethodPerformer performer;
 
     public void send(Photo photo) {
+        log.info("Sending photo: {}", photo);
 
+        performer.perform(SEND_ANIMATION, Map.of(
+                "chat_id", photo.chatId(),
+                "animation", photo.url()
+        ));
     }
 }
