6c8e2f4442ec6c4ed71b722160396c6e89d2397d
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-07-12 11:40:44 +0200

    Init version

---
+++ src/test/java/ua/mibal/bot/QuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mibal.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
