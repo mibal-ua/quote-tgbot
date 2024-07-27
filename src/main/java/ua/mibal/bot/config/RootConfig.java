944ac9301b79b709ff7eeabc015be8520a00008a
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-07-27 13:56:51 +0200

    Disable default chats adding

--- src/main/java/ua/mibal/bot/config/RootConfig.java
+++ src/main/java/ua/mibal/bot/config/RootConfig.java
@@ -1,18 +1,10 @@
 package ua.mibal.bot.config;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ua.mibal.bot.application.QuoteService;
 import ua.mibal.bot.config.props.BotProps;
-import ua.mibal.bot.model.ChatDto;
-
-import java.util.List;
 
 /**
  * @author Mykhailo Balakhon
@@ -26,17 +18,4 @@ import java.util.List;
 })
 @Configuration
 public class RootConfig {
-    private static final Logger log = LoggerFactory.getLogger(RootConfig.class);
-    private final QuoteService quoteService;
-
-    @Bean
-    CommandLineRunner setup() {
-        return args -> List.of(
-                        new ChatDto(623832541, "Anna", "Bybko", "Bybko_Anna", "private"),
-                        new ChatDto(536140066, "Michael Fierce", "Balakhon", "mibal_ua", "private")
-                )
-                .stream()
-                .peek(chat -> log.info("Added default chat {}", chat))
-                .forEach(quoteService::addSubscriber);
-    }
 }
